8ea13edb1f54f3c6f106e0fea6b0345d36dfe063
NicolasT <nicolastem@hotmail.fr>
2020-08-11 21:13:26 +0200

    Default nuxt app

---
+++ nuxt.config.js
@@ -0,0 +1,41 @@
+require('dotenv').config()
+const isDev = process.env.NODE_ENV !== 'production'
+
+export default {
+	mode: 'universal',
+	target: 'static',
+	globalName: process.env.npm_package_name,
+	head: {
+		title: process.env.npm_package_name || '',
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+		],
+		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+	},
+	css: ['reset-css', '~/assets/app.css'],
+	plugins: [{ src: '~/plugins/persistedstate', ssr: false }],
+	components: false,
+	telemetry: false,
+	buildModules: [
+		['@nuxtjs/eslint-module', { cache: true, fix: isDev }],
+		['@nuxtjs/stylelint-module', { fix: true }],
+	],
+	modules: [
+		'@nuxtjs/dotenv',
+		'@nuxtjs/pwa',
+		!isDev ? ['@nuxtjs/google-analytics', { id: process.env.google_analytics }] : '',
+		['@nuxtjs/google-adsense', { id: `ca-pub-${process.env.adsense_id}` }],
+	].filter(Boolean),
+	build: {
+		cache: isDev,
+		postcss: {
+			parser: require('postcss-comment'),
+			plugins: {
+				'postcss-import': {},
+				'postcss-nested': {},
+			},
+		},
+	},
+}
