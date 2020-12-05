8d2c75b70a611aad9e4b447d21577665537d07ea
NicolasT <nicolastem@hotmail.fr>
2020-12-05 18:27:42 +0100

    remove logo

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,37 @@
 <template>
   <div>
-		<div class="container">
-			<div class="row">
-				<div class="col"><h1 class="title">battlepass</h1></div>
+	<div class="container">
+		<div class="row">
+			<div class="col"><h1 class="title">battlepass</h1></div>
+		</div>
+		<div class="row">
+			<div class="col col-3">
+				<input type="text" v-model="level" aria-label="Your level" />
+				<input type="text" v-model="missing" aria-label="Remaining XP" />
 			</div>
-			<div class="row">
-				<div class="col col-3">
-					<input type="text" v-model="level" aria-label="Your level" />
-					<input type="text" v-model="missing" aria-label="Remaining XP" />
-				</div>
-				<div class="col col-3">
-					<personal-stats />
-				</div>
-				<div class="col col-3">
-					<personal-estimates />
-				</div>
-				<div class="col col-3">
-					<games-left />
-				</div>
+			<div class="col col-3">
+				<personal-stats />
+			</div>
+			<div class="col col-3">
+				<personal-estimates />
+			</div>
+			<div class="col col-3">
+				<games-left />
 			</div>
 		</div>
-		<nav class="container-fluid component-select">
-			<a class="selector" :class="{'active': selected === 'chapter' }" @click.prevent="selectComponent('chapter')">Chapter</a>
-			<a class="selector" :class="{'active': selected === 'calendar' }" @click.prevent="selectComponent('calendar')">Calendar</a>
-			<a class="selector" :class="{'active': selected === 'graph' }" @click.prevent="selectComponent('graph')">Progression Graph</a>
-		</nav>
-		<div class="container">
-			<div class="row">
-				<div class="col">
-					<component :is="selectedComponent" />
-				</div>
+	</div>
+	<nav class="container-fluid component-select">
+		<a class="selector" :class="{'active': selected === 'chapter' }" @click.prevent="selectComponent('chapter')">Chapter</a>
+		<a class="selector" :class="{'active': selected === 'calendar' }" @click.prevent="selectComponent('calendar')">Calendar</a>
+		<a class="selector" :class="{'active': selected === 'graph' }" @click.prevent="selectComponent('graph')">Progression Graph</a>
+	</nav>
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				<component :is="selectedComponent" />
 			</div>
 		</div>
+	</div>
   </div>
 </template>
 
